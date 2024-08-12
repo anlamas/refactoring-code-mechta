087191b61ef7ef85c9a78730df79677429206c1e
Aibek Anlamas <aibek.anlamas@gmail.com>
2024-08-12 17:00:07 +0500

    Update readme

--- src/Render.php
+++ src/Render.php
@@ -11,8 +11,8 @@ class Render
     public function renderSearchResult(SearchResult $searchResult): void
     {
         echo "<div class='search-result'>";
-        echo "<h3>" . htmlspecialchars($searchResult->title) . "</h3>";
-        echo "<p>" . htmlspecialchars($searchResult->text) . "</p>";
+        echo "<h3>" . htmlspecialchars($searchResult->title, ENT_QUOTES, 'UTF-8') . "</h3>";
+        echo "<p>" . htmlspecialchars($searchResult->text, ENT_QUOTES, 'UTF-8') . "</p>";
         echo "</div>";
         echo "<hr>";
     }
