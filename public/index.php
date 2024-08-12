d8ae4d06c3278107305e8254e0541d6428cbf667
Aibek Anlamas <aibek.anlamas@gmail.com>
2024-08-12 15:57:52 +0500

    Use refactored SearchService

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use vBulletin\Logger;
+use vBulletin\Render;
+use vBulletin\Search\SearchRequest;
+use vBulletin\Search\SearchService;
+
+require '../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$dbHost = getenv('DB_HOST');
+$dbName = getenv('DB_NAME');
+$dbUser = getenv('DB_USER');
+$dbPassword = getenv('DB_PASS');
+
+$db = new PDO("mysql:dbname={$dbName};host={$dbHost}", $dbUser, $dbPassword);
+$logger = new Logger(getenv('LOG_FILE'));
+$searchService = new SearchService($db, new Render(), $logger);
+
+$request = new SearchRequest((int)$_REQUEST['searchID'] ?? null, $_REQUEST['query'] ?? null);
+$searchService->search($request);
+
+
